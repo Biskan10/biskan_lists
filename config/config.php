46214b3c15020ebb565f246bf58a0d6474b7ed31
Victor Udobong <udobong@usc.edu>
2020-07-28 19:59:20 -0700

    Change the MySQL databse login configurations

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 	// Define constants - constants cannot be changed after they have been declared.
-	define("DB_HOST", "303.itpwebdev.com");
-	define("DB_USER", "udobong_db_user");
+	define("DB_HOST", "biskan10.com");
+	define("DB_USER", "biskan10_master");
 	define("DB_PASS", "5HBAVZvhDmB8xan");
-	define("DB_NAME", "udobong_final_project_db");
+	define("DB_NAME", "biskan_lists");
 ?>
